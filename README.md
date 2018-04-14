00fe1d4abe92aa95d44d2e65453949595554a946
Dima Spitsyn <sqvoch@gmail.com>
2018-04-14 19:28:56 +0300

    v1

---
+++ README.md
@@ -0,0 +1,8 @@
+# dimspitsyn.com
+
+**What is this dimspitsyn.com ?**
+
+My personal website to showcase my interest, work experiences, and projects :)
+
+
+
---
+++ js/countdown.js
@@ -0,0 +1,28 @@
+// set the date we're counting down to
+var countDownDate = new Date("May 1, 2017 15:37:25").getTime();
+
+// update the count down every 1 second
+var countdownfunction = setInterval(function() {
+
+    // get todays date and time
+    var now = new Date().getTime();
+    
+    // find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
